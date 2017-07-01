50daf9af50f211ff3d6e5aba46044679fc4057c5
Marko Niemi <marko.j.niemi@gmail.com>
2017-07-01 16:50:42 +0300

    fix for UserActions and test cases.

--- test/actions/UserActions-test.js
+++ test/actions/UserActions-test.js
@@ -1,5 +1,4 @@
 import {assert} from 'chai';
-import 'isomorphic-fetch';
 import fetchMock from 'fetch-mock';
 import store from '../../src/stores/Store';
 import {addUser, removeUser, editUser, resetUsers} from '../../src/actions/UserActions';
@@ -10,39 +9,45 @@ describe('Action', () => {
     fetchMock.spy();
   });
   afterEach(() => {
+    console.log(fetchMock.calls());
     fetchMock.restore();
     store.dispatch(resetUsers());
   });
   it('should add user in store', () => {
-    fetchMock.once('http://localhost:8080/api/users/', {user: user1});
-    fetchMock.once('http://localhost:8080/api/users/', {user: user2});
-    store.dispatch(addUser(user1));
+    fetchMock.postOnce('http://localhost:8080/api/users/', {user: user1});
+    fetchMock.postOnce('http://localhost:8080/api/users/', {user: user2});
+    store.dispatch(addUser(user1)).then(() => {
+      console.log('addUser');
+      assert.equal(store.getState().users.length, 2);
+    });
     store.dispatch(addUser(user2));
     setTimeout(() => {
+      console.log('timeout callback');
       assert.equal(store.getState().users.length, 2);
       // assert.true(fetchMock.called());
       // assert.isEmpty(fetchMock.calls().unmatched);
-    }, 100);
+    }, 1000);
   });
   it('should remove user from store', () => {
-    fetchMock.once('http://localhost:8080/api/users/', {user: user1});
-    fetchMock.once('http://localhost:8080/api/users/', {user: user2});
+    fetchMock.postOnce('http://localhost:8080/api/users/', {user: user1});
+    fetchMock.postOnce('http://localhost:8080/api/users/', {user: user2});
+    fetchMock.deleteOnce('http://localhost:8080/api/users/1', 200);
     store.dispatch(addUser(user1));
     store.dispatch(addUser(user2));
     store.dispatch(removeUser(user1));
     setTimeout(() => {
       assert.equal(store.getState().users.length, 1);
       // assert.isEmpty(fetchMock.calls().unmatched);
-    }, 100);
+    }, 1000);
   });
   it('should change username in store', () => {
-    fetchMock.once('http://localhost:8080/api/users/', {user: user1});
-    fetchMock.once('http://localhost:8080/api/users/', 200);
+    fetchMock.postOnce('http://localhost:8080/api/users/', {user: user1});
+    fetchMock.putOnce('http://localhost:8080/api/users/1', 200);
     store.dispatch(addUser(user1));
     store.dispatch(editUser({username: 'username', email: 'email', index: 0, _id: '1'}));
     setTimeout(() => {
       assert.equal(store.getState().users[0].username, 'username');
       // assert.isEmpty(fetchMock.calls().unmatched);
-    }, 100);
+    }, 1000);
   });
 });
