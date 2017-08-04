84faa1fadae97b6f9098400ae60d0ed5936571ce
Marko Niemi <marko.j.niemi@gmail.com>
2017-08-04 20:26:16 +0300

    Router moved to index.tsx.

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,26 @@
+import "bootstrap/dist/css/bootstrap.css";
+import "./main.css";
 import * as React from "react";
 import * as ReactDOM from "react-dom";
 import {AppContainer} from "react-hot-loader";
 import {Root} from "./containers/Root";
+import {Provider} from "react-redux";
+import {browserHistory, Route, Router} from "react-router";
+import App from "./components/App";
+import UserContainer from "./components/UserContainer";
+import "./main.css";
+import store from "./stores/Store";
 
 const render = () => {
   ReactDOM.render(
     <AppContainer>
-      <Root />
+      <Provider store={store}>
+        <Router history={browserHistory}>
+          <Route path="/" component={App}/>
+          <Route path="/users/new" component={UserContainer}/>
+          <Route path="/users/:id" component={UserContainer}/>
+        </Router>
+      </Provider>
     </AppContainer>,
     document.getElementById("root"),
   );
