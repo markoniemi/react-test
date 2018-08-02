f64cb420bc879f21d44d81ed7493ba9cdfd1ea08
Marko Niemi <marko.j.niemi@gmail.com>
2018-08-02 09:48:07 +0300

    UserService.

---
+++ server/UserService.ts
@@ -0,0 +1,39 @@
+import {Router} from "express";
+import * as expressRestResource from "express-rest-generator";
+import * as Datastore from "nedb";
+import User from "../src/domain/User";
+export default class UserService {
+  private userRepository: Router;
+  private userDatabase: Datastore;
+
+  constructor() {
+    this.userDatabase = new Datastore();
+    this.userRepository = expressRestResource({db: this.userDatabase});
+  }
+
+  public getUserRepository(): Router {
+    return this.userRepository;
+  }
+
+  // TODO rename -> saveUser/addUser
+  public save(user: User): void {
+    this.userDatabase.insert(user);
+  }
+
+  public delete(): void {
+    this.userDatabase.remove({}, {multi: true});
+  }
+
+  public async findByUsername(username: string): Promise<User> {
+    // nedb uses callback instead of promises, wrap findOne and return Promise
+    return new Promise<User>((resolve, reject) => {
+      this.userDatabase.findOne({username}, (err, doc) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(doc as User);
+        }
+      });
+    });
+  }
+}
